be526443531cd313d4ec3c55fa7f130a9c836d86
Clem's <jeandidier.clement@gmail.com>
2023-05-17 08:48:19 +0200

    ajout constructeur

--- SAE_Graphe/src/Noeud.java
+++ SAE_Graphe/src/Noeud.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class Noeud {
 
+    private String nom;
+
+    private ArrayList<Arc> adj;
+
+    public Noeud(String n){
+        this.nom = n;
+        this.adj = new ArrayList<Arc>();
+    }
 }
