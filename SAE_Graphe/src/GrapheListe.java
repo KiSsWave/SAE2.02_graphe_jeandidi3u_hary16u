72c51754bfd4159adc9a902fe76ec61eaa64429c
Clem's <jeandidier.clement@gmail.com>
2023-05-19 08:07:33 +0200

    ajout méthode

--- SAE_Graphe/src/GrapheListe.java
+++ SAE_Graphe/src/GrapheListe.java
@@ -1,3 +1,43 @@
-public class GrapheListe {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GrapheListe implements Graphe {
+    private ArrayList<String> ensNom;
+    private ArrayList<Noeud> ensNoeuds;
+
+    public GrapheListe() {
+        this.ensNom = new ArrayList<>();
+        this.ensNoeuds = new ArrayList<>();
+    }
+
+    @Override
+    public ArrayList<String> listeNoeuds() {
+        return this.ensNom;
+    }
+
+    @Override
+    public ArrayList<Arc> suivants(String n) {
+        for (int i = 0; i < ensNoeuds.size(); i++) {
+            if (ensNoeuds.get(i).getNom().equals(n)) {
+                return ensNoeuds.get(i).getAdj();
+            }
+        }
+        return null;
+    }
+
+    public void ajouterArc(String depart, String destination, double cout) {
+        if (!ensNom.contains(depart)) {
+            ensNom.add(depart);
+            ensNoeuds.add(new Noeud(depart));
+        }
+        if (!ensNom.contains(destination)) {
+            ensNom.add(destination);
+            ensNoeuds.add(new Noeud(destination));
+        }
+        for (int i = 0; i < ensNoeuds.size(); i++) {
+            if (ensNoeuds.get(i).getNom().equals(depart)) {
+                ensNoeuds.get(i).ajouterArc(destination, cout);
+            }
+        }
+    }
 }
--- SAE_Graphe/src/Main.java
+++ SAE_Graphe/src/Main.java
@@ -10,5 +10,4 @@ public class Main {
         gr.ajouterArc("E", "D", 43);
     }
 }
-    }
-}
+
